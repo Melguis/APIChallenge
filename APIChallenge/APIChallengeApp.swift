ab24b36287915d6c427408b9ac821b5053488aaf
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-18 15:02:06 -0300

    Initial Commit

---
+++ APIChallenge/APIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIChallengeApp.swift
+//  APIChallenge
+//
+//  Created by Jorge Henrique on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct APIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
